9fcf80c936fd98eca42cd0404066ec0a22d0972f
Vitaliy Petrov <v31337@gmail.com>
2015-04-19 10:04:29 +0000

    vagrant

---
+++ cookbooks/meteor_windows/attributes/default.rb
@@ -0,0 +1,13 @@
+default['meteor_windows']['install_meteorite'] = false
+if !node['meteor_windows']['install_meteorite']
+  default['meteor_windows']['meteor_command'] = 'meteor'
+else
+  default['meteor_windows']['meteor_command'] = 'mrt'
+end
+#default['meteor_windows']['create_cmd_files'] = true
+default['meteor_windows']['install_acpipowerbutton'] = false
+default['meteor_windows']['sync_directory'] = '/vagrant'
+default['meteor_windows']['home_directory'] = '/home/vagrant'
+default['meteor_windows']['mount_directory'] = '.meteor/local'
+default['meteor_windows']['owner'] = 'vagrant'
+default['meteor_windows']['group'] = 'vagrant'
